a7e1a929de9954815c9c26e086e35f2eed859307
Alireza Askarpour <askarpourdev@gmail.com>
2024-11-26 12:29:09 -0500

    feat: added refresh token api

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Post, UseInterceptors } from '@nestjs/common';
+import { Request } from 'express';
+import { Body, Controller, Post, Req, UseInterceptors } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignupDto } from './dtos/signup.dto';
 import { TransformInterceptor } from '@src/common/interceptors/transform.interceptor';
@@ -7,6 +8,7 @@ import { LoggerService } from '../logger/logger.service';
 import { LoginDto } from './dtos/login.dto';
 import { ApiLogin } from './docs/login.doc';
 import { Public } from '@src/common/decorators';
+import { RefreshTokenDto } from './dtos/refresh-token.dto';
 
 @UseInterceptors(TransformInterceptor)
 @Controller('auth')
@@ -19,7 +21,7 @@ export class AuthController {
   @Public()
   @ApiSignup()
   @Post('signup')
-  public signup(@Body() signupDto: SignupDto) {
+  public signup(@Body() signupDto: SignupDto): Promise<{ accessToken: string; refreshToken: string }> {
     this.logger.log('Called signup', AuthController.name);
     return this.authService.signup(signupDto);
   }
@@ -27,8 +29,20 @@ export class AuthController {
   @Public()
   @ApiLogin()
   @Post('login')
-  public login(@Body() loginDto: LoginDto) {
+  public login(@Body() loginDto: LoginDto): Promise<{ accessToken: string; refreshToken: string }> {
     this.logger.log('Called login', AuthController.name);
     return this.authService.login(loginDto);
+  }
+
+  @Post('refresh-token')
+  public refreshToken(
+    @Body() { refresh_token }: RefreshTokenDto,
+    @Req() req: Request,
+  ): Promise<{
+    accessToken: string;
+    refreshToken: string;
+  }> {
+    this.logger.log('Called refresh token', AuthController.name);
+    return this.authService.refreshToken(refresh_token, req.headers.origin);
   }
 }
