7c6a061eba20dcabbc3358bf031f84e7b1177b99
Alireza Askarpour <askarpourdev@gmail.com>
2025-03-25 22:16:23 +0330

    feat(chat-messages): add fetchChatHistory repository method

--- src/modules/chat-messages/chat-messages.repository.ts
+++ src/modules/chat-messages/chat-messages.repository.ts
@@ -6,4 +6,13 @@ import { Repository } from 'typeorm';
 @Injectable()
 export class ChatMessagesRepository {
   constructor(@InjectRepository(ChatMessage) private chatMessageEntity: Repository<ChatMessage>) {}
+
+  public async fetchChatHistory(ownerId: string, chatRoomId: string): Promise<ChatMessage[]> {
+    return this.chatMessageEntity.find({
+      where: {
+        owner_id: ownerId,
+        chat_room_id: chatRoomId,
+      },
+    });
+  }
 }
